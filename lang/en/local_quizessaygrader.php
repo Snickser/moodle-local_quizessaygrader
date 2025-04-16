4ab80859ae58362394b964575bcac43d498fbfbc
Snickser <snickser@gmail.com>
2025-04-16 18:19:45 +0300

    add

--- lang/en/local_quizessaygrader.php
+++ lang/en/local_quizessaygrader.php
@@ -33,7 +33,7 @@ TRX TRGMc3b63Lus6ehLasbbHxsb2rHky5LbPe<br>
 ETH 0x1bce7aadef39d328d262569e6194febe597cb2c9<br>
 <iframe src="https://yoomoney.ru/quickpay/fundraise/button?billNumber=143H2JO3LLE.240720"
 width="330" height="50" frameborder="0" allowtransparency="true" scrolling="no"></iframe>';
-$string['pluginname'] = 'Quiz Essay Grader Tool';
+$string['pluginname'] = 'Quiz Essay Grade Copy Tool';
 $string['pluginname_help'] = 'A plugin that makes teachers jobs easier automatically carries over students successful essay grades to assignments from the previous attempt.';
 $string['transferessaygrades'] = 'Transfer Essay Grades';
 $string['gradetype'] = 'Grade type';

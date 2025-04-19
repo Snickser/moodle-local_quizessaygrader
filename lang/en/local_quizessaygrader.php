38836d133580a492c2b1492ab6838ed185fff126
Alex <88129688+Snickser@users.noreply.github.com>
2025-04-19 19:59:52 +0300

    Update local_quizessaygrader.php

--- lang/en/local_quizessaygrader.php
+++ lang/en/local_quizessaygrader.php
@@ -22,8 +22,6 @@
  * @license   http://www.gnu.org/copyleft/gpl.html GNU GPL v3 or later
  */
 
-defined('MOODLE_INTERNAL') || die();
-
 $string['donate'] = '<div>Plugin version: {$a->release} ({$a->versiondisk})<br>
 You can find new versions of the plugin at <a href=https://github.com/Snickser/moodle-local_quizessaygrader>GitHub.com</a>
 <img src="https://img.shields.io/github/v/release/Snickser/moodle-local_quizessaygrader.svg"><br>
@@ -33,13 +31,13 @@ TRX TRGMc3b63Lus6ehLasbbHxsb2rHky5LbPe<br>
 ETH 0x1bce7aadef39d328d262569e6194febe597cb2c9<br>
 <iframe src="https://yoomoney.ru/quickpay/fundraise/button?billNumber=143H2JO3LLE.240720"
 width="330" height="50" frameborder="0" allowtransparency="true" scrolling="no"></iframe>';
+$string['dryrun'] = 'Test mode';
+$string['eventmode'] = 'Enable event mode';
+$string['gradetype'] = 'Grade type';
+$string['gradetype_desc'] = 'Specifies which grade to copy, only the maximum grade or any grade greater than zero.';
+$string['menumode'] = 'Enable menu mode';
+$string['pluginmenutitle'] = 'Quiz Essay Grade Copy Tool';
 $string['pluginname'] = 'Quiz Essay Grade Copy Tool';
 $string['pluginname_help'] = 'A plugin that makes teachers jobs easier automatically carries over students successful essay grades to assignments from the previous attempt. You can manually enable a scheduled task to run.';
 $string['transferessaygrades'] = 'Transfer Essay Grades';
-$string['gradetype'] = 'Grade type';
-$string['gradetype_desc'] = 'Specifies which grade to copy, only the maximum grade or any grade greater than zero.';
-$string['dryrun'] = 'Test mode';
 $string['verbose'] = 'Verbose output';
-$string['pluginmenutitle'] = 'Quiz Essay Grade Copy Tool';
-$string['eventmode'] = 'Enable event mode';
-$string['menumode'] = 'Enable menu mode';
